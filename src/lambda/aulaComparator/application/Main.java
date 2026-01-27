b2892da6258cc2ebdab5a661433192452fff57ff
MarllonAnisio <marllon.anisio@hotmail.com>
2026-01-26 23:50:00 -0300

    formas de usar o lambda

--- src/lambda/aulaComparator/application/Main.java
+++ src/lambda/aulaComparator/application/Main.java
@@ -1,6 +1,7 @@
 package lambda.aulaComparator.application;
 
 import lambda.aulaComparator.entities.Product;
+import utils.PrintarLista;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -22,16 +23,21 @@ public class Main {
         list.add(new Product("lapis", 999.50, true));
         list.add(new Product("lapis de cor", 1.50, true));
 
+        PrintarLista.printLista(list);
+
+
+
         /**
          * Comparator como Classe Anonima:
          *     voce cria uma nova instancia de Comparator e implementa o metodo compare dentro dela.
          * */
-        Comparator<Product> comp = new Comparator<Product>() {
+        Comparator<Product> comparatorClasseAnonima = new Comparator<Product>() {
             @Override
             public int compare(Product o1, Product o2) {
                 return 0;
             }
         };
+        PrintarLista.printLista(list);
 
         /**
          *  Comparator como Objeto de Expressão  lambda Com chaves:
@@ -39,9 +45,31 @@ public class Main {
          *
          *  só que agora voce pode chamar o metodo sort sem precisar criar uma instancia de Comparator.
          * */
-        Comparator<Product> comp1 = (P1, P2) -> {
+        Comparator<Product> comparatorObjetoExpressaoComChaves = (P1, P2) -> {
             return P1.getName().toUpperCase().compareTo(P2.getName().toUpperCase());
         };
+        list.sort(comparatorObjetoExpressaoComChaves);
+        PrintarLista.printLista(list);
 
+        /**
+         * e uma forma mais limpa e simples.
+         *         quando so se tem uma linha de codigo, o return e chaves podem ser descartados.
+         * */
+        Comparator<Product> comparatorObjetoExpressaoSemChaves = (P1, P2) -> P1.getName().toUpperCase().compareTo(P2.getName().toUpperCase());
+        list.sort(comparatorObjetoExpressaoSemChaves);
+        PrintarLista.printLista(list);
+
+        /**
+         *  melhorando ainda mais, ele pode apenas ser dentro no sort, veja:
+         *  (p1, p2) isso são os produtos, os parametros do "metodo"
+         *   -> isso é chamado de ArrowFunction porque parece uma flexa, lol
+         *   depois do arrowFunction vem o "corpo do metodo"
+         *   algo que vai ser testado, consumido, execultado, podendo ou não ter retorno(proximas aulas)
+         *
+         * */
+        list.sort((p1, p2) -> p1.getName().toUpperCase()
+                .compareTo(p2.getName().toUpperCase()
+                ));
+        PrintarLista.printLista(list);
     }
 }
