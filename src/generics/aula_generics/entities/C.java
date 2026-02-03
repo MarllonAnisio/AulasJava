7fe920f5048c6dd6bca30063f6ecc2f5bc13417d
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-02 23:45:20 -0300

    classe C que herda de Produto, seria um produto C

--- src/generics/aula_generics/entities/C.java
+++ src/generics/aula_generics/entities/C.java
@@ -1,4 +1,12 @@
 package generics.aula_generics.entities;
 
-public class C {
+public class C  extends Product{
+    public C(String field, double v) {
+        super(field, v);
+    }
+
+    @Override
+    public String toString() {
+        return "c []" + super.toString();
+    }
 }
