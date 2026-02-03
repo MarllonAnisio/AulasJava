62d8eb2d61dc1250b7e8357a58b93baaa25009c0
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-02 23:43:56 -0300

    classe produto, abstrata e usando comparable

---
+++ src/generics/aula_generics/entities/Product.java
@@ -0,0 +1,35 @@
+package generics.aula_generics.entities;
+
+public abstract class Product implements Comparable<Product>{
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + "]";
+    }
+
+
+    @Override
+    public int compareTo(Product other) {
+        return this.price.compareTo(other.getPrice());
+    }
+}
