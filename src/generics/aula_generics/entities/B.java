52374524b830ccd0aa0f65629e04ea7611bbb429
MarllonAnisio <marllon.anisio@hotmail.com>
2026-02-02 23:46:05 -0300

    classe B que herda de Produto, seria um produto B

--- src/generics/aula_generics/entities/B.java
+++ src/generics/aula_generics/entities/B.java
@@ -1,4 +1,13 @@
 package generics.aula_generics.entities;
 
-public class B {
+public class B extends Product{
+
+    public B(String field, double v) {
+        super(field, v);
+    }
+
+    @Override
+    public String toString() {
+        return "b []" + super.toString();
+    }
 }
